37ea590c52086278daeba23678556f84a7485130
xB0bon <pan.bobon@gmail.com>
2024-04-12 18:43:33 +0200

    commit

--- TypeCasting/Program.cs
+++ TypeCasting/Program.cs
@@ -6,12 +6,13 @@
         {
             double pi = 3.14;
             int pi_1 = Convert.ToInt32(pi); // convert from double (3.14) to int (3)
-            int liczba = 123;
+            
 
             String x = "true";
             bool y = Convert.ToBoolean(x);
             
-            String text = Convert.ToString(liczba);
+            int liczba = 123;
+            String text = Convert.ToString(liczba); // convert from int to string
             
             Console.WriteLine(pi_1);
             Console.WriteLine(text);
