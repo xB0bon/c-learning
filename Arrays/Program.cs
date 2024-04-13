48be3597f8281ead002ae8bbfbf8058608e7c85b
xB0bon <pan.bobon@gmail.com>
2024-04-14 00:36:35 +0200

    14.04.2024

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cars = { "BMW", "AUDI", "TESLA", "FORD" };
+            // metoda 1
+            /*
+            
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            */
+            //metoda 2
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+        }
+    }
+};
---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            String name = Console.ReadLine() ?? "";
+            while (name == "")
+            {
+                Console.Write("Name: ");
+                name = Console.ReadLine() ?? "";
+            }
+            Console.Write("How many times  you want to sing birthday song?");
+            int times = Convert.ToInt32(Console.ReadLine());
+            HappyBirthday(times, name);
+
+        }
+
+        static void HappyBirthday(int times, String name)
+        
+        {
+            for (int i = 1; i < times; i++)
+            {
+                Console.WriteLine("Happy Birthday to you!");
+                Console.WriteLine("Happy Birthday to you!");
+                Console.WriteLine($"Happy Birthday to dear {name}!");
+                Console.WriteLine("Happy Birthday to you!");
+            }
+            
+        }
+    }
+};
