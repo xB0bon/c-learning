07c619bb48848f0251d8c2bf7c81e5d6e1fa791e
xB0bon <pan.bobon@gmail.com>
2024-04-15 23:05:14 +0200

    15/04/2024

---
+++ ConditionalOperator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = 20;
+            string message;
+            
+            // wersja 1
+            
+            if (temperature >= 15)
+            {
+                message = "It's warm outside !";
+            }
+            else
+            {
+                message = "It's cold outside !";
+            }
+
+            Console.WriteLine(message);
+            
+            // wersja 2
+            
+            message = temperature >= 15 ? "It's warm outside !" : "It's cold outside !";
+
+            Console.WriteLine(message);
+            
+            
+
+
+        }
+
+        
+        
+
+        
+    }
+};
---
+++ Exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+
+            try
+            {
+                Console.WriteLine("Enter number 1:");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter number 2:");
+                y = Convert.ToDouble(Console.ReadLine());
+                double result = x / y;
+                Console.WriteLine($"{x} / {y} = {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter ONLY numbers!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for visiting!");
+            }
+
+            
+        }
+
+        
+        
+
+        
+    }
+};
